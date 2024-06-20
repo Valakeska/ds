60d2a7b219b644e17ccd63a4356cf7ebaa9071c7
RuslanaBelyakova <deep-crusher@mail.ru>
2024-06-21 00:36:32 +0300

    firts commit

---
+++ pizdecsykablyat/SqlClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pizdecsykablyat
+{
+    internal class SqlClass
+    {
+        public SqlConnection sqlConnection = new SqlConnection("Data Source=DESKTOP-KBH6SHR\\SQLEXPRESS;Initial Catalog=skyf;Integrated Security=True;Encrypt=False");
+
+        public DataTable Commands(string selectSQL)
+        {
+            DataTable dataTable = new DataTable("dataBase");
+
+            sqlConnection.Open();
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();
+            sqlCommand.CommandText = selectSQL;
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+            sqlDataAdapter.Fill(dataTable);
+            sqlConnection.Close();
+            return dataTable;
+        }
+    }
+}
